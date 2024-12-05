d8148e1deadd6fae44c5260c967314a21e7df162
Esmael-Mohammed <esmaelmohammed01@gmail.com>
2024-12-05 16:49:15 +0300

    add express module

--- app.js
+++ app.js
@@ -2,3 +2,11 @@ const express=require('express');
 const cors=require('cors');
 const app=express();
 
+const port=5000;
+app.listen(port,(error)=>{
+    if(error){
+        console.log(error)
+    }else{
+       console.log(`listing on ${port}`) 
+    }
+})
